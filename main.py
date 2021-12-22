8d453b5d9452121dfd0b46c990ad8e59d47feb2f
Filip <filip.oroz@fsre.sum.ba>
2021-12-23 00:36:53 +0100

    Assets grouping and minor changes

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import pygame
 import math
 import random
+import os
 from pygame import mixer  #import za zvukove
-pygame.mixer.init()   #import za zvukove
+pygame.mixer.init()       #import za zvukove
 
 #setup prozora programa
 pygame.init() 
 WIDTH, HEIGHT = 800, 500                           #inicijalizacija varijabli koje ću koristiti kao visinu i širinu prozora programa. Velikim slovom jer su to konstantne i neće se mijenjati nikako
 PROZOR = pygame.display.set_mode((WIDTH, HEIGHT))  #inicijalizacija prozora 
-pygame.display.set_caption("HANGMAN PYGAME")       #naslov prozora programa
+pygame.display.set_caption("HANGMAN PYGAME - FILIP OROZ")       #naslov prozora programa                                   
 
-pozadinski_zvuk = pygame.mixer.music.load ("pozadinski.mp3")  #pozadinska muzika
-pygame.mixer.music.play(-1)                                   #pozadinska muzika na loop 
+#pozadinska muzika
+music = pygame.mixer.music.load(os.path.join("zvukovi", 'pozadinski.mp3')) #odabir pjesme iz direktorija "zvukovi"
+pygame.mixer.music.play(-1)                                                #pozadinska muzika na loop 
+
 
 # varijable za buttone
 RADIUS = 20
 RAZMAK = 15
 slova = []  #lista u koju ću staviti sva slova 
-startx = round((WIDTH - (RADIUS * 2 + RAZMAK) * 13) / 2)
-starty = 400 
+startx = round((WIDTH - (RADIUS * 2 + RAZMAK) * 13) / 2) #koordinata x
+starty = 400 #koordinata y
 A = 65 #prvo slovo
 
 for i in range (26):
@@ -29,14 +32,15 @@ for i in range (26):
 
 #fontovi
 slova_font = pygame.font.SysFont('comicsans', 40)            #font i veličina slova
-rijec_font = pygame.font.SysFont('dejavusansmono', 45)       #font i veličina rijeci
+rijec_font = pygame.font.SysFont('dejavusansmono', 40)       #font i veličina rijeci
 naslov_font = pygame.font.SysFont('comicsans', 70)           #font i veličina naslova
+potpis_font = pygame.font.SysFont('javanesetext', 65)           #font i veličina potpisa
 
 
 #učitavanje slika
 images = [] #lista slika
 for i in range(7):
-    image = pygame.image.load("hangman" + str(i) + ".png")
+    image = pygame.image.load(os.path.join("Slike","hangman" + str(i) + ".png"))
     images.append(image)          #dodavanje slike u listu
 
 
@@ -50,6 +54,7 @@ pogodeno = []  #lista slova koja su pogodena
 #boje
 BIJELA = (255,255,255)  #varijabla bijele RGB boje
 CRNA = (0,0,0)          #varijabla crne RGB boje
+POTPIS_BOJA = (31,206,170) #varijabla boje potpisa
 
 
 # setup game loop
@@ -92,6 +97,8 @@ def ispis_poruke(poruka):    #metoda s parametrom poruka
      PROZOR.fill(BIJELA)     #ispuni ekran samo bijelom bojom
      text = rijec_font.render(poruka, 1, CRNA) #render teksta
      PROZOR.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2 )) #blitanje na ekran
+     potpis = potpis_font.render("Filip Oroz", 1 , POTPIS_BOJA)    #render texta za potpis
+     PROZOR.blit(potpis, (WIDTH/2 - potpis.get_width()/2, 395 )) #blitanje texta potpisa na prozor
      pygame.display.update()  #update ekrana
      pygame.time.delay(3000)  #3sec delay, 3 sec trajanje teksta 
 
@@ -112,10 +119,10 @@ while run:          #dok je god varijabla run true, petlja se izvrsava
                         slovo[3] = False     #ne pokazuj slovo
                         pogodeno.append(ltr) #dodaj ga u listu slova "pogodeno"
                         if ltr in riječ:
-                            pogodak = mixer.Sound('pogodak.wav')  #inicijalizacija zvuka pogotka
+                            pogodak = mixer.Sound(os.path.join("zvukovi",'pogodak.wav'))  #inicijalizacija zvuka pogotka
                             pogodak.play() #puštanje zvuka pogotka
                         if ltr not in riječ: #ako odabrano slovo nije u traženoj riječi
-                            kiks = mixer.Sound('pogresno_slovo.wav')  #inicijalizacija zvuka pogreške
+                            kiks = mixer.Sound(os.path.join("zvukovi",'pogresno_slovo.wav'))  #inicijalizacija zvuka pogreške
                             kiks.play() #puštanje zvuka pogreške
                             hangman_status += 1 #povecaj hangman_status za 1, tj. promijeni sliku iz liste za 1
     
@@ -128,13 +135,15 @@ while run:          #dok je god varijabla run true, petlja se izvrsava
             break #izlazak iz petlje
     
     if pobjeda:
-        win = mixer.Sound('win.wav')  #inicijalizacija zvuka pobjede
+        pygame.mixer.music.stop()
+        win = mixer.Sound(os.path.join("zvukovi",'win.wav'))  #inicijalizacija zvuka pobjede
         win.play() #puštanje zvuka pobjede
         ispis_poruke("Pobjeda!") #metoda s parametrom pobjeda
         break
 
     if hangman_status == 6:
-        gameover = mixer.Sound('gameover.wav')  #inicijalizacija gameover zvuka   
+        pygame.mixer.music.stop()
+        gameover = mixer.Sound(os.path.join("zvukovi",'gameover.wav'))  #inicijalizacija gameover zvuka   
         gameover.play() #puštanje gameover zvuka
         ispis_poruke("Izgubili ste!") #metoda s parametrom izgubili ste
         break 
