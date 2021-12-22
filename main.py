9d25d556f99c31160c5d4fb6b8ddfbc3bd6ad6bc
Filip <filip.oroz@fsre.sum.ba>
2021-12-23 00:06:11 +0100

    Sound effects added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 import random
+from pygame import mixer  #import za zvukove
+pygame.mixer.init()   #import za zvukove
 
 #setup prozora programa
 pygame.init() 
@@ -8,13 +10,16 @@ WIDTH, HEIGHT = 800, 500                           #inicijalizacija varijabli ko
 PROZOR = pygame.display.set_mode((WIDTH, HEIGHT))  #inicijalizacija prozora 
 pygame.display.set_caption("HANGMAN PYGAME")       #naslov prozora programa
 
+pozadinski_zvuk = pygame.mixer.music.load ("pozadinski.mp3")  #pozadinska muzika
+pygame.mixer.music.play(-1)                                   #pozadinska muzika na loop 
+
 # varijable za buttone
 RADIUS = 20
 RAZMAK = 15
 slova = []  #lista u koju ću staviti sva slova 
 startx = round((WIDTH - (RADIUS * 2 + RAZMAK) * 13) / 2)
 starty = 400 
-A = 65
+A = 65 #prvo slovo
 
 for i in range (26):
     x = startx + RAZMAK * 2 + ((RADIUS * 2 + RAZMAK) * ( i % 13))  #i%13 daje ostatak 
@@ -23,30 +28,28 @@ for i in range (26):
 
 
 #fontovi
-slova_font = pygame.font.SysFont('comicsans', 40) #font slova
-rijec_font = pygame.font.SysFont('dejavusansmono', 45) #font rijeci
-naslov_font = pygame.font.SysFont('comicsans', 70) #font naslova
+slova_font = pygame.font.SysFont('comicsans', 40)            #font i veličina slova
+rijec_font = pygame.font.SysFont('dejavusansmono', 45)       #font i veličina rijeci
+naslov_font = pygame.font.SysFont('comicsans', 70)           #font i veličina naslova
 
 
 #učitavanje slika
 images = [] #lista slika
 for i in range(7):
     image = pygame.image.load("hangman" + str(i) + ".png")
-    images.append(image)                    #dodavanje slike u listu
-
-
+    images.append(image)          #dodavanje slike u listu
 
 
 # game varijable
-hangman_status = 0     # varijabla da znam koja mi slika treba 
+hangman_status = 0     # varijabla pogriješenih slova da znam koja mi slika treba 
 riječi = ["PYTHON", "HANGMAN", "PYGAME", "ETHEREUM"] #lista riječi za pogadanje
-riječ = random.choice(riječi) #rijec je random izabrana iz liste riječi
-pogodeno = [] #lista slova koja su pogodena
+riječ = random.choice(riječi) #riječ je random izabrana iz liste riječi
+pogodeno = []  #lista slova koja su pogodena
 
 
 #boje
 BIJELA = (255,255,255)  #varijabla bijele RGB boje
-CRNA = (0,0,0)  #varijabla crne RGB boje
+CRNA = (0,0,0)          #varijabla crne RGB boje
 
 
 # setup game loop
@@ -54,17 +57,17 @@ FPS = 60  #frames per second
 clock = pygame.time.Clock() 
 run = True
 
-def draw():       #funkcija za crtanje
+def draw():                #funkcija za crtanje
     PROZOR.fill((BIJELA))  #postavljanje boje prozora, sa bojom u RGB
 
-    text = naslov_font.render("Dobrodošli!", 1 , CRNA)
-    PROZOR.blit(text, (WIDTH/2 - text.get_width()/2, 20))
+    text = naslov_font.render("Dobrodošli!", 1 , CRNA)    #render texta
+    PROZOR.blit(text, (WIDTH/2 - text.get_width()/2, 20)) #blitanje texta na prozor
 
     # draw loop
     display_word = ""
     for slovo in riječ:
         if slovo in pogodeno:
-            display_word += slovo + " " #iduće slovo da bude odvojeno od proslog zato imam space
+            display_word += slovo + " " #iduće slovo da bude odvojeno od proslog zato space
         else: #ako nije pogodeno
             display_word += "_ " #nepogodeni
 
@@ -84,34 +87,36 @@ def draw():       #funkcija za crtanje
     pygame.display.update()  #update prozora
 
 
-def ispis_poruke(poruka):  #metoda s parametrom poruka 
+def ispis_poruke(poruka):    #metoda s parametrom poruka 
      pygame.time.delay(1000) #delay 1 sec, 1000 ms
-     PROZOR.fill(BIJELA)  #ispuni ekran samo bijelom bojom
+     PROZOR.fill(BIJELA)     #ispuni ekran samo bijelom bojom
      text = rijec_font.render(poruka, 1, CRNA) #render teksta
      PROZOR.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2 )) #blitanje na ekran
-     pygame.display.update() #update ekrana
+     pygame.display.update()  #update ekrana
      pygame.time.delay(3000)  #3sec delay, 3 sec trajanje teksta 
 
 
-while run:  #dok je god varijabla run true, petlja se izvrsava
+while run:          #dok je god varijabla run true, petlja se izvrsava
     clock.tick(FPS) #otkucaji FPS brzinom
     
-
     for event in pygame.event.get():  #svaki event koji bude će se dodati
         if event.type == pygame.QUIT: # provjerava je li kliknuto X na prozoru gašenje programa
             run = False               # ako je kliknuto, onda varijablu run stavlja na false i gasi petlju
-        if event.type == pygame.MOUSEBUTTONDOWN: #ako je kliknuto mišem 
+        if event.type == pygame.MOUSEBUTTONDOWN:  #ako je kliknuto mišem 
             miš_x, miš_y = pygame.mouse.get_pos() #dobivanje koordinata miša
             for slovo in slova:
                 x,y,ltr, vidljivo = slovo
                 if vidljivo:
                     dis = math.sqrt((x - miš_x) **2 + (y - miš_y) **2 ) #dobivanje distance izmedu misa i slova
                     if dis < RADIUS:
-                        slovo[3] = False #ne pokazuj slovo
-                        #ovdje win zvuk
+                        slovo[3] = False     #ne pokazuj slovo
                         pogodeno.append(ltr) #dodaj ga u listu slova "pogodeno"
+                        if ltr in riječ:
+                            pogodak = mixer.Sound('pogodak.wav')  #inicijalizacija zvuka pogotka
+                            pogodak.play() #puštanje zvuka pogotka
                         if ltr not in riječ: #ako odabrano slovo nije u traženoj riječi
-                            #ovdje lose zvuk
+                            kiks = mixer.Sound('pogresno_slovo.wav')  #inicijalizacija zvuka pogreške
+                            kiks.play() #puštanje zvuka pogreške
                             hangman_status += 1 #povecaj hangman_status za 1, tj. promijeni sliku iz liste za 1
     
     draw()   #pozivanje funkcije draw
@@ -123,15 +128,15 @@ while run:  #dok je god varijabla run true, petlja se izvrsava
             break #izlazak iz petlje
     
     if pobjeda:
-        #zvuk pobjede
+        win = mixer.Sound('win.wav')  #inicijalizacija zvuka pobjede
+        win.play() #puštanje zvuka pobjede
         ispis_poruke("Pobjeda!") #metoda s parametrom pobjeda
         break
 
     if hangman_status == 6:
-        #gameover zvuk
+        gameover = mixer.Sound('gameover.wav')  #inicijalizacija gameover zvuka   
+        gameover.play() #puštanje gameover zvuka
         ispis_poruke("Izgubili ste!") #metoda s parametrom izgubili ste
         break 
 
-# pos = pygame.mouse.get_pos() #pozicija miša
-            # print(pos) #printaj poziciju miša [u pygame gornji lijevi kut je (0, 0), gornji desni (max , 0)]
 pygame.quit()
